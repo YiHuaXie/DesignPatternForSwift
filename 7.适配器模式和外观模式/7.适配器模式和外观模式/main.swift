4cd9cba1dd7d9a2f33f0958c8dab613d53f2fb87
NeroXie <xyh30902@163.com>
2020-06-05 15:07:01 +0800

    适配器模式

---
+++ 7.适配器模式和外观模式/7.适配器模式和外观模式/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  7.适配器模式和外观模式
+//
+//  Created by NeroXie on 2020/6/3.
+//  Copyright © 2020 NeroXie. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
